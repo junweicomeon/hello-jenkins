65f544e03f50c27fb6557260ec8e3e6b31134c02
zhujunwei <junweicomeon@163.com>
2018-07-26 23:16:27 +0800

    删除方法

--- src/main/java/com/baidu/controller/HelloWorldController.java
+++ src/main/java/com/baidu/controller/HelloWorldController.java
@@ -17,10 +17,7 @@ public class HelloWorldController {
     public String showMenu(){
         return "hello_world";
     }
-	@RequestMapping(value = "/sayHello2",method = RequestMethod.GET)
-    public String showMenu1(){
-        return "hello_world2";
-    }
+	
 
 
 }
